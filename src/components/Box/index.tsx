3eb453cc8337bc9aad3bfdbfdb5ed6d1712f003d
felipemarinhodev <felipmarinho@gmail.com>
2022-09-20 15:11:28 -0700

    a1.5 - create the Box component

---
+++ src/components/Box/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+interface StyleSheet {
+  fontFamily: string;
+}
+
+interface BoxProps {
+  tag: any; // TODO: set a type
+  children: React.ReactNode;
+  styleSheet: StyleSheet;
+}
+
+export default function Box({ styleSheet, children, tag }: BoxProps) {
+  const Tag = tag || "div";
+
+  return <Tag style={styleSheet}>{children}</Tag>;
+}
