5c6b7576e262462d49714ce69fc224933f9de3b1
felipemarinhodev <felipmarinho@gmail.com>
2022-09-20 16:25:57 -0700

    a1.7 - using BaseComponent

--- src/components/Box/index.tsx
+++ src/components/Box/index.tsx
@@ -1,8 +1,6 @@
+import { BaseComponent } from "@src/theme/BaseComponent";
+import { StyleSheet } from "@src/theme/StyleSheet";
 import React from "react";
-
-interface StyleSheet {
-  fontFamily: string;
-}
 
 interface BoxProps {
   tag: any; // TODO: set a type
@@ -10,8 +8,12 @@ interface BoxProps {
   styleSheet: StyleSheet;
 }
 
-export default function Box({ styleSheet, children, tag }: BoxProps) {
+export default function Box({ styleSheet, children, tag, ...props }: BoxProps) {
   const Tag = tag || "div";
 
-  return <Tag style={styleSheet}>{children}</Tag>;
+  return (
+    <BaseComponent as={Tag} styleSheet={styleSheet} {...props}>
+      {children}
+    </BaseComponent>
+  );
 }
