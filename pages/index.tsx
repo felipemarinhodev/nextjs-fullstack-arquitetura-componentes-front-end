322f5711214036fcc06d4b5c3bf1114e85350055
felipemarinhodev <felipmarinho@gmail.com>
2022-09-20 16:28:49 -0700

    a1.7 - improve styles using StyleSheet related to BaseComponents

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,26 @@
 import Box from "@src/components/Box";
 import theme from "@src/theme/theme";
+import styled from "styled-components";
+
+const MyComponent = styled.div`
+  color: purple;
+  font-weight: 700;
+`;
 
 export default function HomeScreen() {
   return (
-    <Box tag="main" styleSheet={{ fontFamily: theme.typography.fontFamily }}>
-      oi
+    <Box
+      tag="main"
+      styleSheet={{
+        fontFamily: theme.typography.fontFamily,
+        backgroundColor: {
+          xs: "red",
+          sm: "yellow",
+          md: "blue",
+        },
+      }}
+    >
+      <MyComponent>oi</MyComponent>
     </Box>
   );
 }
