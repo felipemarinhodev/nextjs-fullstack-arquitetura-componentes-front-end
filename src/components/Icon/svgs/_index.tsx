6d94336ddbc5adccd48a7617ae44ed990b960b3d
felipemarinhodev <felipmarinho@gmail.com>
2022-09-22 07:27:43 -0700

    a3.2 - export icons

---
+++ src/components/Icon/svgs/_index.tsx
@@ -0,0 +1,16 @@
+export { default as arrowup } from "./icon_arrowup";
+export { default as clock_fill } from "./icon_clock_fill";
+export { default as coffee } from "./icon_coffee";
+export { default as default } from "./icon_default";
+export { default as github } from "./icon_github";
+export { default as home } from "./icon_home";
+export { default as instagram } from "./icon_instagram";
+export { default as linkedin } from "./icon_linkedin";
+export { default as menu } from "./icon_menu";
+export { default as profile } from "./icon_profile";
+export { default as school } from "./icon_school";
+export { default as search } from "./icon_search";
+export { default as settings } from "./icon_settings";
+export { default as terminal } from "./icon_terminal";
+export { default as twitter } from "./icon_twitter";
+export { default as youtube } from "./icon_youtube";
