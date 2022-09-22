b9e26eadfafc9ff4cdc128225ff6233a8ac18373
felipemarinhodev <felipmarinho@gmail.com>
2022-09-21 18:18:22 -0700

    a2.2 - create Menu component

--- src/screens/HomeScreen/patterns/Menu/index.tsx
+++ src/screens/HomeScreen/patterns/Menu/index.tsx
@@ -1,5 +1,10 @@
 import Box from "@src/components/Box";
+import Text from "@src/components/Text";
 
 export default function Menu() {
-  return <Box>Menu</Box>;
+  return (
+    <Box>
+      <Text>Menu</Text>
+    </Box>
+  );
 }
