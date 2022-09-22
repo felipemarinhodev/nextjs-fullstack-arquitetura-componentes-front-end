07ddf855fe87241f7e53fcf1dabe2aa54d84dae3
felipemarinhodev <felipmarinho@gmail.com>
2022-09-21 21:59:22 -0700

    a2.4 - improve component to accept new props

--- src/components/Text/index.tsx
+++ src/components/Text/index.tsx
@@ -1,15 +1,28 @@
+import { BaseComponent } from "@src/theme/BaseComponent";
+import { variantsTypes } from "@src/theme/defaults/typography";
+import { StyleSheet } from "@src/theme/StyleSheet";
 import theme from "@src/theme/theme";
 import React from "react";
-import Box from "../Box";
 
 interface TextProps {
   tag?: "p" | "li" | "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "span";
   children?: React.ReactNode;
+  variant?: variantsTypes;
+  styleSheet?: StyleSheet;
 }
 
-export default function Text(props: TextProps) {
+export default function Text({ styleSheet, variant, ...props }: TextProps) {
+  const textVariant = theme.typography.variants[variant];
+
   return (
-    <Box styleSheet={{ fontFamily: theme.typography.fontFamily }} {...props} />
+    <BaseComponent
+      styleSheet={{
+        fontFamily: theme.typography.fontFamily,
+        ...textVariant,
+        styleSheet,
+      }}
+      {...props}
+    />
   );
 }
 
