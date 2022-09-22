d74b6eb6c4b49326ca228cdc032d6c8c5b0e6515
felipemarinhodev <felipmarinho@gmail.com>
2022-09-21 18:19:39 -0700

    a2.2 - add Feed component in screen

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -1,5 +1,7 @@
 import Box from "@src/components/Box";
+import Text from "@src/components/Text";
 import Background from "./patterns/Background";
+import Feed from "./patterns/Feed";
 import Menu from "./patterns/Menu";
 
 export default function HomeScreen() {
@@ -7,6 +9,11 @@ export default function HomeScreen() {
     <Box tag="main">
       <Background />
       <Menu />
+      <Feed>
+        <Feed.Header />
+        <Text>Últimas Atualizações</Text>
+        <Feed.Posts />
+      </Feed>
     </Box>
   );
 }
