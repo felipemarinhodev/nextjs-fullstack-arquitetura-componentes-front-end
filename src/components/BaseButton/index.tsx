57a8aa3eaeb2bc5c7401ba1e31ee6e0cfd08f689
felipemarinhodev <felipmarinho@gmail.com>
2022-09-22 23:26:37 -0700

    a4.3 - create button components

---
+++ src/components/BaseButton/index.tsx
@@ -0,0 +1,63 @@
+import { StyleSheet } from "@src/theme/StyleSheet";
+import { ThemeTypographyVariants } from "@src/theme/theme";
+import { useRouter } from "next/router";
+import React from "react";
+import { useRipple } from "react-use-ripple";
+import styled from "styled-components";
+import Text from "../Text";
+
+interface BaseButtonProps {
+  children: React.ReactNode;
+  href?: string;
+  textVariant?: ThemeTypographyVariants;
+  styleSheet?: StyleSheet;
+  onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
+}
+
+const StyledButton = styled(Text)<any>``;
+
+export default function BaseButton({
+  children,
+  textVariant,
+  styleSheet,
+  href,
+  ...props
+}: BaseButtonProps) {
+  const ref = React.useRef();
+  useRipple(ref, {
+    animationLength: 600,
+    rippleColor: "rgba(255, 255, 255, 0.7)",
+  });
+  const router = useRouter();
+
+  const isLink = Boolean(href);
+
+  const Tag = isLink ? "a" : "button";
+  return (
+    <StyledButton
+      ref={ref}
+      tag={Tag}
+      href={href}
+      {...props}
+      styleSheet={{
+        border: 0,
+        backgroundColor: "transparent",
+        color: "inherit",
+        outline: "0",
+        cursor: "pointer",
+        textDecoder: "none",
+        ...styleSheet,
+      }}
+      onClick={(event) => {
+        isLink && event.preventDefault();
+        isLink && router.push(href);
+      }}
+    >
+      {children}
+    </StyledButton>
+  );
+}
+
+BaseButton.defaultProps = {
+  textVariant: "body2",
+};
---
+++ src/components/Button/index.tsx
@@ -0,0 +1,11 @@
+import BaseButton from "../BaseButton";
+
+export default function Button() {
+  return (
+    <BaseButton textVariant="body2">
+      Botão Master Botão que parece botão
+    </BaseButton>
+  );
+}
+
+Button.Base = BaseButton;
