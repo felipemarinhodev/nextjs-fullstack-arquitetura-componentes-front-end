11d4dcc01e70eca6a38245d24f5cad9508d7470b
felipemarinhodev <felipmarinho@gmail.com>
2022-09-23 09:42:39 -0700

    a5.2 - add Feed component

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -1,11 +1,8 @@
 import Box from "@src/components/Box";
-import Link from "@src/components/Link";
-import Text from "@src/components/Text";
 import { useTheme } from "@src/theme/ThemeProvider";
 import Background from "./patterns/Background";
 import Feed from "./patterns/Feed";
 import Footer from "./patterns/Footer";
-import Menu from "./patterns/Menu";
 
 export default function HomeScreen() {
   const theme = useTheme();
@@ -19,19 +16,9 @@ export default function HomeScreen() {
       }}
     >
       <Background />
-      {/* <Link colorVariant="negative" href="/sobre">
-        Vá para a página Sobre
-      </Link>
-      <Link href="https://google.com">Ir para o Google</Link>
-      <Menu />
       <Feed>
         <Feed.Header />
-        <Text tag="h2" variant="heading2">
-          Últimas Atualizações
-        </Text>
-        <Feed.Posts />
       </Feed>
-    */}
       <Footer />
     </Box>
   );
