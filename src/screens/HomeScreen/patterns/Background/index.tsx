611e1ba26d9ecf6412d50b1582eb68c1203071f5
felipemarinhodev <felipmarinho@gmail.com>
2022-09-23 09:27:31 -0700

    a5.2 - add image of background

--- src/screens/HomeScreen/patterns/Background/index.tsx
+++ src/screens/HomeScreen/patterns/Background/index.tsx
@@ -1,10 +1,18 @@
 import Box from "@src/components/Box";
-import Text from "@src/components/Text";
 
 export default function Background() {
+  const imageUrl =
+    "https://images.unsplash.com/photo-1650661926447-9efb2610f64c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1473&q=80";
+
   return (
-    <Box>
-      <Text>Background</Text>
-    </Box>
+    <Box
+      styleSheet={{
+        width: "100%",
+        height: "400px",
+        backgroundImage: `url("${imageUrl}")`,
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+      }}
+    />
   );
 }
