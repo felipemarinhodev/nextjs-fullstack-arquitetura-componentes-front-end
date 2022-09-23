0f07470ba8782bd35a8aeab4fb28ba3ed4b1d3ff
felipemarinhodev <felipmarinho@gmail.com>
2022-09-23 08:18:14 -0700

    a4.4 - implement functionality fullWidth

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,11 +1,32 @@
-import BaseButton from "../BaseButton";
+import React from "react";
+import BaseButton, { BaseButtonProps } from "../BaseButton";
 
-export default function Button() {
+interface ButtonProps extends BaseButtonProps {
+  children: React.ReactNode;
+  fullWidth?: boolean;
+}
+export default function Button({
+  children,
+  styleSheet,
+  fullWidth,
+}: ButtonProps) {
   return (
-    <BaseButton textVariant="body2">
-      Botão Master Botão que parece botão
+    <BaseButton
+      styleSheet={{
+        alignSelf: "flex-start",
+        ...(fullWidth && {
+          alignSelf: "initial",
+        }),
+        ...styleSheet,
+      }}
+    >
+      {children}
     </BaseButton>
   );
 }
+
+Button.defaultProps = {
+  fullWidth: false,
+};
 
 Button.Base = BaseButton;
