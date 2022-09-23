7dfc15e3e2bb77dbe3209126419d525a8df0dc4f
felipemarinhodev <felipmarinho@gmail.com>
2022-09-22 17:05:36 -0700

    a4.2 - create Sobre page

---
+++ pages/sobre.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Link from "@src/components/Link";
+
+export default function AboutScreen() {
+  return (
+    <div>
+      Sobre!
+      <Link href="/">Voltar para home</Link>
+    </div>
+  );
+}
