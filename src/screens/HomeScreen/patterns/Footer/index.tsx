e28a160fb8b410bc4be89a2d0f8b62ec7556fbf9
felipemarinhodev <felipmarinho@gmail.com>
2022-09-23 09:33:52 -0700

    a5.2 - update Footer component

--- src/screens/HomeScreen/patterns/Footer/index.tsx
+++ src/screens/HomeScreen/patterns/Footer/index.tsx
@@ -1,10 +1,23 @@
 import Box from "@src/components/Box";
 import Text from "@src/components/Text";
+import { useTheme } from "@src/theme/ThemeProvider";
 
 export default function Footer() {
+  const theme = useTheme();
   return (
-    <Box>
-      <Text>Footer</Text>
+    <Box
+      styleSheet={{
+        backgroundColor: theme.colors.neutral.x900,
+        color: theme.colors.neutral.x000,
+        height: "120px",
+        width: "100%",
+        paddingHorizontal: "24px",
+        paddingVertical: "24px",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text variant="body2">Feito por Felipe Marinho</Text>
     </Box>
   );
 }
