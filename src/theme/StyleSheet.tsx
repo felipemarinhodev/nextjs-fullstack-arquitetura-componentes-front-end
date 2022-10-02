27437cfefb51a62b1f620c32d475ae716b6306a0
felipemarinhodev <felipmarinho@gmail.com>
2022-10-01 22:49:34 -0700

    extends CSSProperties

--- src/theme/StyleSheet.tsx
+++ src/theme/StyleSheet.tsx
@@ -1,8 +1,10 @@
+import { CSSProperties } from "react";
 import { Breakpoints } from "@skynexui/responsive_stylesheet";
 
 type ResponsiveProperty<Type> = Partial<Record<Breakpoints, Type>>;
 
-export interface StyleSheet {
+export interface StyleSheet
+  extends Omit<CSSProperties, "backgroundColor" | "fontFamily"> {
   fontFamily?: ResponsiveProperty<string> | string;
   backgroundColor?: ResponsiveProperty<string> | string;
   [key: string]: any;
