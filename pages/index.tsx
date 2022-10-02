9b744068f1583db58cae67841b4dc2a836461893
felipemarinhodev <felipmarinho@gmail.com>
2022-10-02 09:26:59 -0700

    a3.3 - get all posts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,15 @@
+import PostsService from "@src/services/posts/PostsService";
 import { withTemplateConfig } from "@src/services/template/withTemplateConfig";
 
 export { default } from "@src/screens/HomeScreen";
 
 export async function getStaticProps() {
+  const posts = await PostsService().getAll();
+  console.log("Posts", posts);
+
   return {
-    props: await withTemplateConfig({}),
+    props: await withTemplateConfig({
+      posts,
+    }),
   };
 }
