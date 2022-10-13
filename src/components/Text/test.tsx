c04f9198367f40814319e8084dc1991e58134d5c
felipemarinhodev <felipmarinho@gmail.com>
2022-10-13 08:35:33 -0700

    add test

--- src/components/Text/test.tsx
+++ src/components/Text/test.tsx
@@ -1,10 +1,11 @@
-import { render, screen } from '@testing-library/react';
+import { renderWithTheme } from '@src/utils/tests/helpers';
+import { screen } from '@testing-library/react';
 
 import Text from '.';
 
 describe('<Text />', () => {
   it('should render the text', () => {
-    render(<Text tag="h1">React avançado</Text>);
+    renderWithTheme(<Text tag="h1">React avançado</Text>);
     expect(
       screen.getByRole('heading', { name: /react avançado/i })
     ).toBeInTheDocument();
