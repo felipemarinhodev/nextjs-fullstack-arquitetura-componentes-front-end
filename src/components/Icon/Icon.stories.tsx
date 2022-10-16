c6b5167a3939f40779a13d7566083f1a374ea470
felipemarinhodev <felipmarinho@gmail.com>
2022-10-16 10:57:49 -0700

    add tools icon in the list

--- src/components/Icon/Icon.stories.tsx
+++ src/components/Icon/Icon.stories.tsx
@@ -1,32 +1,33 @@
-import { ComponentMeta, ComponentStory } from "@storybook/react";
-import Icon, { IconProps, iconSizes } from ".";
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import Icon, { IconProps, iconSizes } from '.';
 
 const sizeOptions = Object.keys(iconSizes);
 
 export default {
-  title: "Components/Icon",
+  title: 'Components/Icon',
   args: {},
   argTypes: {
-    size: { control: "select", options: sizeOptions },
+    size: { control: 'select', options: sizeOptions },
     name: {
-      control: "select",
+      control: 'select',
       options: [
-        "arrowup",
-        "clock_fill",
-        "coffee",
-        "default",
-        "github",
-        "home",
-        "instagram",
-        "linkedin",
-        "menu",
-        "profile",
-        "school",
-        "search",
-        "settings",
-        "terminal",
-        "twitter",
-        "youtube",
+        'arrowup',
+        'clock_fill',
+        'coffee',
+        'default',
+        'github',
+        'home',
+        'instagram',
+        'linkedin',
+        'menu',
+        'profile',
+        'school',
+        'search',
+        'settings',
+        'terminal',
+        'tools',
+        'twitter',
+        'youtube',
       ],
     },
   },
@@ -40,6 +41,6 @@ const Template: ComponentStory<typeof Icon> = (args) => {
 export const Default = Template.bind({});
 
 Default.args = {
-  size: "md",
-  name: "default",
+  size: 'md',
+  name: 'default',
 } as IconProps;
